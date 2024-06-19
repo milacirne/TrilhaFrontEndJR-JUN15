ec5ddf49613a146ef32b05e278e35571f26c9d41
Camila Cirne <camila.cirne22@gmail.com>
2024-06-19 19:37:07 -0300

    Ajustes na semantica do HTML

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 
   return (
     <S.Container>
-      <div className="grid-item">
+      <section className="grid-item">
         <Title>FICHA DE PERSONAGEM</Title>
         <Avatar />
         <Field />
@@ -28,8 +28,8 @@ const Home = () => {
         <StatusBar progress={50} icon={defense}></StatusBar>
         <StatusBar progress={80} icon={mana}></StatusBar>
         <StatusBar progress={85} icon={initiative} isLastBar></StatusBar>
-      </div>
-      <div className="grid-item">
+      </section>
+      <section className="grid-item">
         <div className="subtitle-container">
           <Subtitle>A jornada por trás da heroína</Subtitle>
           <img src={trainer} className="gif"/>
@@ -38,8 +38,8 @@ const Home = () => {
         <Text>Após uma jornada enriquecedora na Irlanda, onde aprimorou seu inglês e ampliou seus horizontes, Camila decidiu trilhar um novo caminho: o Desenvolvimento Front-End.</Text>
         <Text>Determinada a dominar o código, ela iniciou sua jornada com um curso de Desenvolvimento Full Stack. Sua mente ágil, alimentada por cursos online e bootcamps, equipou-a para construir um futuro brilhante no mundo digital.</Text>
         <Text isLastText>O objetivo de Camila hoje é evoluir e se tornar uma mestra do código, pronta para desvendar os segredos da programação e criar soluções inovadoras para ajudar as pessoas. Sua paixão pela tecnologia e sua mente perspicaz são suas maiores armas, guiando-a em sua aventura épica.</Text>
-      </div>
-      <div className="grid-item">
+      </section>
+      <section className="grid-item">
         <Subtitle>Missões e Motivações</Subtitle>
         <Text isFirstText>
           Ao se unir à comunidade Codigo Certo Coders, Camila vê uma oportunidade única de evoluir suas habilidades mágicas na programação. Desenvolver alianças estratégicas com outros magos tecnológicos e enfrentar desafios épicos fortalecerá seu caminho rumo à maestria.
@@ -47,11 +47,11 @@ const Home = () => {
         <Text isLastText>
           Camila sente um chamado para usar suas habilidades de magia tecnológica em prol da sociedade. Ela acredita firmemente que o conhecimento técnico pode ser uma ferramenta poderosa para resolver problemas e melhorar vidas.
         </Text>
-      </div>
-      <div className="grid-item">
+      </section>
+      <section className="grid-item">
         <Subtitle>Paixões da Aventureira</Subtitle>
         <Hobbie />
-      </div>
+      </section>
     </S.Container>
   )
 }
